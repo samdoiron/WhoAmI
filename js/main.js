2460425850f8b6612078a4d262a86c97d191dc8a
Conlectus <sam.doiron96@gmail.com>
2014-06-10 11:30:51 -0300

    Prevent graph from expanding excessively

--- js/main.js
+++ js/main.js
@@ -70,21 +70,27 @@ $(function () {
         });
 
         pie.hover(function () {
-            this.sector.stop();
-            this.sector.scale(1.1, 1.1, this.cx, this.cy);
+          this.sector.stop();
 
-            if (this.label) {
-                this.label[0].stop();
-                this.label[0].attr({ r: 7.5 });
-                this.label[1].attr({ "font-weight": 800 });
-            }
+          this.sector.transform('s1 1 ' + this.cx, this.cy);
+          this.sector.scale(1.1, 1.1, this.cx, this.cy);
+
+          if (this.label) {
+              this.label[0].stop();
+              this.label[0].attr({ r: 7.5 });
+              this.label[1].attr({ 'font-weight': 800 });
+          }
+
         }, function () {
-            this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+          var self = this;
+          this.sector.animate({
+              transform: 's1 1 ' + this.cx + ' ' + this.cy
+          }, 500, "bounce");
 
-            if (this.label) {
-                this.label[0].animate({ r: 5 }, 500, "bounce");
-                this.label[1].attr({ "font-weight": 400 });
-            }
+          if (this.label) {
+              this.label[0].animate({ r: 5 }, 500, 'bounce');
+              this.label[1].attr({ "font-weight": 400 });
+          }
         });
         document.body.appendChild(ol);
         return;
